e6e72c7cf363799c9037eb29f88ff1c84d82c998
bhargav1240 <bhavu92@gmail.com>
2020-12-25 12:06:56 +0530

    factory

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -28,6 +28,19 @@ class UserFactory extends Factory
             'email_verified_at' => now(),
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
             'remember_token' => Str::random(10),
+            'is_admin' => false
         ];
     }
+
+    public function isAdmin(){
+        return $this->state([
+            'is_admin' => true
+        ]);
+    }
+
+    //php artisan tinker
+    //use App\Models\User; ===> it will import User model
+    //User::factory()->make(); ===> it will make one new user
+    //User::factory(3)->make(); ===> it will make 3 new user
+    //User::factory()->isAdmin()->make(); ===> it will make one new user with is_admin = true
 }
